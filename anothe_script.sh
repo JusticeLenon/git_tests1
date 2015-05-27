0efbfd9879694c0f40cd9c869b635db920cf327d
Justice Stoute <Justice.Lenon@cix.csi.cuny.edu>
2015-05-27 10:23:13 -0400

    Added another loop.

--- anothe_script.sh
+++ anothe_script.sh
@@ -1,21 +1,20 @@
-#Create one file
-
+#Create one 
 for i in {1..100000}; 
 do
-    if [ $i == 5 ] then
+    if [ $i == 5 ]; then
 	touch file$i.txt
     fi  	
 done
 
-echo$i
+echo $i
 
 # Put some contents into the file
 
 for j in {1..10}; 
 do
-    if [ $j == 5 ] then
+    if [ $j == 5 ]; then
 	echo "Conents of file $j" > files$j.txt
     fi  	
 done
 
-echo$j
+echo $j
